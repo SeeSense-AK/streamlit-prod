bee2d1581ffd2cc916b787cfb1af4af4b56a44b5
SeeSense-AK <abhishek@seesense.cc>
2025-07-11 11:26:24 +0100

    Create __init__.py

---
+++ app/components/__init__.py
@@ -0,0 +1,27 @@
+"""
+Reusable UI components for SeeSense Dashboard
+Charts, maps, metrics cards, filters, and other UI elements
+"""
+
+# When we implement these components, we'll add imports like:
+# from .metrics_cards import metric_card, kpi_grid, trend_indicator
+# from .maps import hotspot_map, route_popularity_map, density_heatmap
+# from .charts import time_series_chart, correlation_heatmap, bar_chart, scatter_plot
+# from .filters import date_range_filter, spatial_filter, category_filter
+
+__all__ = [
+    # Placeholder for future component modules
+    # "metric_card",
+    # "kpi_grid", 
+    # "trend_indicator",
+    # "hotspot_map",
+    # "route_popularity_map",
+    # "density_heatmap",
+    # "time_series_chart",
+    # "correlation_heatmap",
+    # "bar_chart",
+    # "scatter_plot",
+    # "date_range_filter",
+    # "spatial_filter",
+    # "category_filter"
+]
