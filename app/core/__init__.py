8babb62ef7c19b12da9cd32527064049ee52d9b1
SeeSense-AK <abhishek@seesense.cc>
2025-07-17 15:10:48 +0100

    dy

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,28 +1,14 @@
 """
 Core business logic modules for SeeSense Dashboard
-Data processing, ML models, analytics, and insights generation
 """
 
 from .data_processor import data_processor, DataProcessor
-
-# When we implement these modules, we'll add imports like:
-# from .ml_models import RiskPredictor, SafetyClusterer, AnomalyDetector
-# from .analytics import TimeSeriesAnalyzer, CorrelationAnalyzer
-# from .spatial_analyzer import HotspotAnalyzer, RouteOptimizer
-# from .insights_generator import InsightsEngine, RecommendationEngine
+from .metrics_calculator import metrics_calculator
+from .groq_insights_generator import create_insights_generator
 
 __all__ = [
     "data_processor",
     "DataProcessor",
-    
-    # Placeholder for future ML/Analytics modules
-    # "RiskPredictor",
-    # "SafetyClusterer", 
-    # "AnomalyDetector",
-    # "TimeSeriesAnalyzer",
-    # "CorrelationAnalyzer",
-    # "HotspotAnalyzer",
-    # "RouteOptimizer",
-    # "InsightsEngine",
-    # "RecommendationEngine"
+    "metrics_calculator",
+    "create_insights_generator"
 ]
